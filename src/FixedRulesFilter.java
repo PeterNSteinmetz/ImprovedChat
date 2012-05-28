a662df449ca4087d289830bfe3adb9ad34537d1a
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2012-05-28 15:31:50 -0700

    Add rules for prefix and allowed if prefix present.
    Change-Id: If162055396e9d4be4f323a3ce276637c56bad1ca

--- src/FixedRulesFilter.java
+++ src/FixedRulesFilter.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -11,33 +13,65 @@ import java.util.regex.PatternSyntaxException;
  */
 public class FixedRulesFilter implements LineFilter {
 
-	Pattern mandatoryMatchPattern;
+	Pattern prefixPattern;
+	ArrayList<Pattern> allowPatterns;
 	
 	/**
 	 * Build for a regex string.
 	 */
-	public FixedRulesFilter(String regex) {
-		try {
-			mandatoryMatchPattern = Pattern.compile(regex);
-		} catch (PatternSyntaxException pse) {
-			;	// at least don't crash
-		}
+	public FixedRulesFilter() {
+		allowPatterns = new ArrayList<Pattern>();
 	}
 	
 	/**
-	 * Determine if line is valid by checking if 
-	 * contains a pattern.
+	 * Determine if line is valid by whether it contains
+	 * the prefix, and if it does, it it contains one of the allowed 
+	 * patterns.
 	 */
 	@Override
 	public boolean isValid(String line) {
-		return mandatoryMatchPattern.matcher(line).find();
+		if (prefixPattern.matcher(line).find()) {
+			Iterator<Pattern> patIter = allowPatterns.iterator();
+			while (patIter.hasNext()) {
+				if (patIter.next().matcher(line).find()) return true;
+			}
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public boolean isIgnored(String line) {
 		return !isValid(line);
 	}
-
+	
+	/**
+	 * Set the prefix pattern, which if matched, requires matching the 
+	 * allowed patterns.
+	 */
+	public void setPrefixPattern(String prePat) {
+		System.out.println("prePat=/"+prePat+"/");
+		try {
+			Pattern pat = Pattern.compile(prePat);
+			prefixPattern = pat;
+		} catch (PatternSyntaxException pse) {
+			; // don't crash
+		}
+	}
+	
+	/**
+	 * Add an allow pattern which will be checked if the prefix matches.
+	 */
+	public void addAllowPattern(String allowPat) {
+		try {
+			Pattern pat = Pattern.compile(allowPat);
+			allowPatterns.add(pat);
+		} catch (PatternSyntaxException pse) {
+			; // don't crash
+		}
+	}
+	
+	
 	@Override
 	public List<Pattern> getTrackPatterns() {
 		return null;
